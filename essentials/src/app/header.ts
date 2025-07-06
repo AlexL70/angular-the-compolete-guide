6a7a49af85a519c65e19e96a375f3df56f1899e8
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-06 13:04:44 +0300

    12. Creating a First Custom Component 13. [Optional] JavaScript Refresher: Classes, Properties & More 14. Configuring the Custom Component

---
+++ essentials/src/app/header.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    standalone: true, // is set to true by default in Angular 19 or higher
+    templateUrl: './header.html',
+})
+export class Header { }
