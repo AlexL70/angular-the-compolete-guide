457bd727512f4b45fe27056aadadd032363c4eb2
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-06 20:32:24 +0300

    37. Exercise: Create a Configurable Component

--- essentials/src/app/app.ts
+++ essentials/src/app/app.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { User } from "./user/user";
+import { Tasks } from './tasks/tasks';
 import { DUMMY_USERS } from './dummy-users';
+import { SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [RouterOutlet, Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'Angular Essentials';
   users = DUMMY_USERS;
+  selectedUserId: string = DUMMY_USERS[0].id;
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log(`Selected user ID: ${id}`);
+    this.selectedUserId = id;
   }
 }
