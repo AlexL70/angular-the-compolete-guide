f6b19c9c065a7b3882ca62c7ac54c185e95f7bdb
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-06 18:15:12 +0300

    29. We Need More Flexible Components! 30. Defining Component Inputs

--- essentials/src/app/user/user.ts
+++ essentials/src/app/user/user.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,17 +7,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.scss'
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => {
-    return `assets/users/${this.selectedUser().avatar}`;
-  });
-
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser().avatar}`;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+  onSelectUser() {
+    console.log(`Selected user: ${this.name}`);
+   }
 }
