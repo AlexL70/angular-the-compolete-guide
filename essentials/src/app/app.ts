d1000cc6fff44135b6de2480b044d6b430067d9b
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-08 07:36:27 +0300

    Revert "43. Legacy Angular: Using ngFor & ngIf" - use modern directives instead
    This reverts commit 9f011b05db210862b0a6301a393d933e955d0977.

--- essentials/src/app/app.ts
+++ essentials/src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common'
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { User } from "./user/user";
@@ -8,7 +7,7 @@ import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User, Tasks, NgFor, NgIf],
+  imports: [RouterOutlet, Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
