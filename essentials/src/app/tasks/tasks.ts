9067743e8b5bc46d10a2be86235728a30ce5d92f
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-08 08:08:14 +0300

    45. Outputting User-specific Tasks

--- essentials/src/app/tasks/tasks.ts
+++ essentials/src/app/tasks/tasks.ts
@@ -10,4 +10,33 @@ import { Task } from "./task/task";
 })
 export class Tasks {
   @Input({required: true}) userName!: string;
+  @Input({ required: true }) userId!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Complete Angular Essentials Course',
+      summary: 'Finish the course and apply the knowledge in a project.',
+      time: new Date('2025-08-01T10:00:00'),
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2026-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2027-06-15',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
