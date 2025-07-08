d91571f0c9def1beee3b6ddeea8c85caf99cb67c
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-08 07:54:59 +0300

    44. Adding More Components to the Demo App

--- essentials/src/app/tasks/tasks.ts
+++ essentials/src/app/tasks/tasks.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from "./task/task";
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule],
+  imports: [CommonModule, Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss'
 })
