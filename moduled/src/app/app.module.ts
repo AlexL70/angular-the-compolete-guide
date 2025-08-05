e8ebc2fd6afa8f24c520c446b60d233a0e54f9d0
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-08-05 17:29:24 +0300

    72. Creating & Using Shared Modules

--- moduled/src/app/app.module.ts
+++ moduled/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { User } from './user/user';
 import { Tasks } from './tasks/tasks';
 import { Task } from './tasks/task/task';
 import { NewTask } from './tasks/new-task/new-task';
-import { Card } from './shared/card/card';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-    declarations: [App, Header, User, Card, Tasks, Task, NewTask],
+    declarations: [App, Header, User, Tasks, Task, NewTask],
     bootstrap: [App],
-    imports: [BrowserModule, CommonModule, FormsModule],
+    imports: [BrowserModule, CommonModule, FormsModule, SharedModule],
     providers: [],
 })
 export class AppModule {
